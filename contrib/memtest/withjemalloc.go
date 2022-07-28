deb13b3e0e00156beec585c8cfeb9427fbac5d5d
NateScarlet <NateScarlet@Gmail.com>
2022-07-28 15:40:23 +0800

    fix: glog goroutine leak fix: should not write to stdout on import

--- contrib/memtest/withjemalloc.go
+++ contrib/memtest/withjemalloc.go
@@ -4,7 +4,6 @@ package main
 
 import (
 	"github.com/dgraph-io/ristretto/z"
-	"github.com/golang/glog"
 )
 
 func Calloc(size int) []byte { return z.Calloc(size, "memtest") }
@@ -13,14 +12,10 @@ func NumAllocBytes() int64   { return z.NumAllocBytes() }
 
 func check() {
 	if buf := z.CallocNoRef(1, "memtest"); len(buf) == 0 {
-		glog.Fatalf("Not using manual memory management. Compile with jemalloc.")
+		log.Fatalf("Not using manual memory management. Compile with jemalloc.")
 	} else {
 		z.Free(buf)
 	}
 
 	z.StatsPrint()
-}
-
-func init() {
-	glog.Infof("USING JEMALLOC")
 }
